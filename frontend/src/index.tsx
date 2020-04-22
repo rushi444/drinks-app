1b7edd1fcd32e88755ff56abbaf4c5af34c22f62
rushi444 <rushi44@gmail.com>
2020-04-22 12:39:03 -0700

    Front end themes and provider setup

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { CSSReset, ThemeProvider } from '@chakra-ui/core';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { ApolloLink, from } from 'apollo-link';
+
+import { App } from './App';
+import { customTheme } from './theme';
+
+const httpLink = new HttpLink({
+  uri: 'https://localhost:4000/graphql',
+  credentials: 'include',
+});
+
+const authMiddleware = new ApolloLink((operation, forward) => {
+  const token = localStorage.getItem('token') || null;
+  if (token) {
+    operation.setContext({
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    });
+  }
+  return forward(operation);
+});
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: from([authMiddleware, httpLink]),
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={customTheme}>
+        <CSSReset />
+        <App />
+      </ThemeProvider>
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
