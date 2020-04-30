965f8cf945840299f32171bba9598f7203d1e057
rushi444 <rushi44@gmail.com>
2020-04-30 15:02:01 -0700

    some more styling :lipstick:

--- frontend/src/components/Comments.tsx
+++ frontend/src/components/Comments.tsx
@@ -3,6 +3,7 @@ import { Box, Input, Button } from '@chakra-ui/core';
 import { IComment } from '../types';
 import { useMutation } from '@apollo/react-hooks';
 import { CREATE_COMMENT } from '../graphql';
+import styled from '@emotion/styled';
 
 interface IProps {
   comments: IComment[];
@@ -13,27 +14,50 @@ export const Comments: FC<IProps> = ({ comments, recipeId }) => {
   const [text, setText] = useState('');
   const [createComment] = useMutation(CREATE_COMMENT, {
     variables: {
-      recipeId, text
-    }, onCompleted: () => setText('')
+      recipeId,
+      text,
+    },
+    onCompleted: () => setText(''),
   });
   return (
     <Box>
-      <h1>Comments</h1>
+      <Box as='h1' fontSize='lg'>Comments</Box>
       <Box display='flex'>
         <Input
           type='text'
           name='text'
+          size='sm'
           placeholder='add a comment...'
           value={text}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setText(e.target.value)}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setText(e.target.value)
+          }
         />
-        <Button onClick={(): any => createComment()}>Post</Button>
+        <Button
+          size='sm'
+          variant='outline'
+          variantColor='purple'
+          onClick={(): any => createComment()}>
+          Post
+        </Button>
       </Box>
-      {comments?.map((comment, index) => (
-        <Box key={index}>
-          {comment.text} By: {comment.createdBy.name}
-        </Box>
-      ))}
+      <Box height='auto'>
+        {comments?.map((comment, index) => (
+          <CommentContainer
+            key={index}
+            display='flex'
+            justifyContent='space-between'>
+            <Box>{comment.text}</Box>
+            <Box>By: {comment.createdBy.name}</Box>
+          </CommentContainer>
+        ))}
+      </Box>
     </Box>
   );
 };
+
+const CommentContainer = styled(Box)`
+display: flex;
+justify-content: space-between;
+margin-top: 1%;
+`;
--- frontend/src/components/Ingredients.tsx
+++ frontend/src/components/Ingredients.tsx
@@ -10,12 +10,13 @@ export const Ingredients: FC<IProps> = ({ ingredients }) => {
   return (
     <Box>
       <h1>Ingredients</h1>
-      <List styleType='disc'></List>
-      {ingredients?.map((ingredient, index) => (
-        <ListItem key={index}>
-          {ingredient.amount} {ingredient.name}
-        </ListItem>
-      ))}
+      <List spacing='1%' padding='1%' styleType='square'>
+        {ingredients?.map((ingredient, index) => (
+          <ListItem key={index}>
+            {ingredient.amount} {ingredient.name}
+          </ListItem>
+        ))}
+      </List>
     </Box>
   );
 };
